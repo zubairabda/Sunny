dad3981fa59502438299f84c516851e9a3e62c9d
Zubair <71045824+zubairabda@users.noreply.github.com>
2025-10-12 17:04:32 -0400

    DMA cleanup

--- src/dma.c
+++ src/dma.c
@@ -183,6 +183,17 @@ static s32 dma_select_channel(void)
     return selected;
 }
 
+static void dma_schedule_next_transfer(void)
+{
+    g_dma.active_channel = -1;
+    g_dma.event = 0;
+    s32 next_channel = dma_select_channel();
+    if (next_channel >= 0)
+    {
+        g_dma.event = schedule_event(dma_callback_id, next_channel, 0);
+    }
+}
+
 static void dma_handle_next_transfer(u32 channel, s32 ticks_late)
 {
     struct dma_port *port = &g_dma.ports[channel];
@@ -260,16 +271,9 @@ static void dma_handle_next_transfer(u32 channel, s32 ticks_late)
 
                 g_dma.ports[channel].control &= ~(0x1000000);
                 dma_set_interrupt(channel);
-
-                g_dma.active_channel = -1;
-                g_dma.event = 0;
                 transfer->in_progress = false;
 
-                s32 next_channel = dma_select_channel();
-                if (next_channel >= 0)
-                {
-                    g_dma.event = schedule_event(dma_callback_id, next_channel, 0);
-                }
+                dma_schedule_next_transfer();
                 return;
             }
             
@@ -297,16 +301,9 @@ static void dma_handle_next_transfer(u32 channel, s32 ticks_late)
             //debug_log("[DMA] finished transfer for channel %u\n", channel);
             port->control &= ~(0x1000000);
             dma_set_interrupt(channel);
-
-            g_dma.active_channel = -1;
-            g_dma.event = 0;
             transfer->in_progress = false;
 
-            s32 next_channel = dma_select_channel();
-            if (next_channel >= 0)
-            {
-                g_dma.event = schedule_event(dma_callback_id, next_channel, 0);
-            }
+            dma_schedule_next_transfer();
         }
     }
     else
@@ -315,14 +312,8 @@ static void dma_handle_next_transfer(u32 channel, s32 ticks_late)
         transfer->pending = true;
         transfer->pending_words = transfer_size;
 
-        // NOTE: since some games interrupt an MDECin transfer with an MDECout one, we need to also handle channel switching here
-        g_dma.active_channel = -1;
-        g_dma.event = 0;
-        s32 next_channel = dma_select_channel();
-        if (next_channel >= 0)
-        {
-            g_dma.event = schedule_event(dma_callback_id, next_channel, 0);
-        }
+        // since some games interrupt an MDECin transfer with an MDECout one, we need to also handle channel switching here
+        dma_schedule_next_transfer();
     }
 
     // NOTE: if priorities are changed during a transfer, it should be checked here
@@ -458,15 +449,8 @@ void dma_write(u32 offset, u32 value)
                     {
                         SY_ASSERT(g_dma.event);
                         remove_event(g_dma.event);
-                        g_dma.event = 0;
-                        // since the next channel to transfer relies on the current channel to re-select a channel,
-                        // we need to do it here
-                        g_dma.active_channel = -1;
-                        s32 next_channel = dma_select_channel();
-                        if (next_channel >= 0)
-                        {
-                            g_dma.event = schedule_event(dma_callback_id, next_channel, 0);
-                        }
+                        // since the next channel to transfer relies on the current channel to re-select a channel, we need to do it here
+                        dma_schedule_next_transfer();
                     }
                 }
             }
