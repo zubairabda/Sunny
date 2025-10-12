ebc001e08de79d5d1dd1185fe09fbdc99f02a8a4
Zubair <71045824+zubairabda@users.noreply.github.com>
2025-10-12 17:44:11 -0400

    Remove deprecated files

--- bin/shader/compile.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-for %%f in (*.vert) do (glslangValidator -V %%f -o %%~nf.spv)
-for %%f in (*.frag) do (glslangValidator -V %%f -o %%~nf.spv)
-
-pause
--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem build script
-
-clang makefile.c -O0 -g -std=c17 -o makefile.exe -D_CRT_SECURE_NO_WARNINGS
-
-if %ERRORLEVEL% == 0 (
-    makefile
-)
